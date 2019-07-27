e76d09e97a92e7a0046dc39a4691f436184d953a
tjslzz <3244188372@qq.com>
2019-07-27 16:21:43 +0800

    [jerryLi]  feat : add Login view

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="LoginForm">
+    <el-form
+      :model="ruleForm"
+      status-icon
+      :rules="rules"
+      ref="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
+    >
+      <el-form-item label="用户名" prop="userName">
+        <el-input
+          type="text"
+          v-model="ruleForm.userName"
+          autocomplete="off"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input
+          type="password"
+          v-model="ruleForm.password"
+          autocomplete="off"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')"
+          >提交</el-button
+        >
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+  data() {
+    return {
+      ruleForm: {
+        userName: "",
+        password: ""
+      },
+      rules: {
+        userName: [
+          {
+            required: true,
+            message: "请输入姓名",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          //   console.log(this.$refs[formName].model);
+        } else {
+          this.$message.error("错了哦，这是一条错误消息");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,29 @@
 <template>
-  <div id="login">
-    <h1>Login</h1>
-  </div>
+  <el-row id="login">
+    <el-col :span="8" :offset="8">
+      <div class="grid-content">
+        <el-button circle class="login_button">登录</el-button>
+        <LoginForm />
+      </div>
+    </el-col>
+  </el-row>
 </template>
+
 <script>
+import LoginForm from "../components/LoginForm";
 export default {
-  name: "login"
+  name: "login",
+  components: {
+    LoginForm
+  }
 };
 </script>
+
+<style scoped>
+.login_button {
+  width: 100px;
+  height: 100px;
+  background: #bfdfff;
+  margin-bottom: 50px;
+}
+</style>
