2a12e81df84cea4813eb68d89171f9b9dd4d67d3
FelicityChen <felicity.chen@oocl.com>
2019-07-31 07:23:18 +0800

    [Felicity Chen]refator: pretty the mobile login UI

--- src/pages/mobile/customer/CustomerLoginMobile.vue
+++ src/pages/mobile/customer/CustomerLoginMobile.vue
@@ -1,55 +1,45 @@
 <template>
-  <div style="text-align: center;margin-top: 100px">
-    <el-card class="box-card">
-      <div class="block">
-        <el-avatar :size="80" :src="circleUrl"></el-avatar>
-      </div>
+  <div>
+    <nav-bar title="智能派单停车系统" class="nav-bar"></nav-bar>
+    <div style="text-align: center;margin-top: 100px" class="loginMainPanel">
       <br />
       <div style="text-align: center">
-        <el-form
-          :model="user"
-          :rules="rules"
-          ref="loginForm"
-          label-width="100px"
-        >
+        <div class="block">
+          <el-avatar :size="80">
+            <i class="el-icon-user"></i>
+          </el-avatar>
+        </div>
+        <el-form :model="user" :rules="rules" ref="loginForm" label-width="100px">
           <el-form-item label="用户名" prop="userName">
-            <el-input
-              type="text"
-              v-model="user.userName"
-              autocomplete="off"
-            ></el-input>
+            <el-input type="text" v-model="user.userName" autocomplete="off"></el-input>
           </el-form-item>
           <el-form-item label="密码" prop="password">
-            <el-input
-              type="password"
-              v-model="user.password"
-              autocomplete="off"
-            >
-            </el-input>
+            <el-input type="password" v-model="user.password" autocomplete="off"></el-input>
           </el-form-item>
           <el-form-item>
             <el-button
+              class="sumbitButton"
               type="primary"
               @keydown.enter="submitForm('loginForm')"
               @click="submitForm('loginForm')"
-              >提交</el-button
-            >
-            <el-button @click="resetForm('loginForm')">重置</el-button><br />
-            <a href="#" @click="locationRegister">注册</a>
+            >提交</el-button>
+            <el-button @click="resetForm('loginForm')" class="resetButton">重置</el-button>
+            <br />
+            <div @click="locationRegister" class="registerButton">注册</div>
           </el-form-item>
         </el-form>
       </div>
-    </el-card>
+    </div>
   </div>
 </template>
 
 <script>
+import { NavBar } from "vant";
 import Constant from "@/common/constance.js";
 export default {
   name: "CustomerLoginMobile",
   data() {
     return {
-      circleUrl: ".././assets/head.jpg",
       user: {
         userName: "",
         password: ""
@@ -99,11 +89,25 @@ export default {
     locationRegister() {
       this.$router.push("/register-user");
     }
+  },
+  components: {
+    NavBar
   }
 };
 </script>
 
 <style scoped>
+.block {
+  margin-bottom: 20px;
+}
+.loginMainPanel {
+  width: 90%;
+  margin: 0 auto;
+}
+>>> .el-form {
+  padding-right: 10%;
+  margin-left: -8%;
+}
 .text {
   font-size: 14px;
 }
@@ -117,5 +121,47 @@ export default {
 >>> .element.style {
   width: 313px;
   padding-right: 70px;
+}
+
+>>> .el-avatar {
+  background: #409eff;
+  font-size: 26px;
+}
+
+.sumbitButton {
+  float: left;
+  line-height: 12px;
+  width: 47%;
+}
+
+.resetButton {
+  float: right;
+  line-height: 12px;
+  width: 47%;
+}
+
+.registerButton {
+  background-color: white;
+  line-height: 35px;
+  width: 100%;
+  color: black;
+  border-radius: 5px;
+  border: 1px solid #409eff;
+  margin-top: 10px;
+}
+</style>
+<style lang="less" scoped>
+.van-nav-bar {
+  height: 50px;
+  background-color: #409eff;
+  color: white;
+}
+
+&.van-nav-bar__text {
+  color: white;
+}
+
+&.van-nav-bar__title {
+  color: white;
 }
 </style>
