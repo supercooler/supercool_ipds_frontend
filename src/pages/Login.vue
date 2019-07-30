8737b6b410fe457631e018fbe8cb8807e912343b
tjslzz <3244188372@qq.com>
2019-07-30 09:43:24 +0800

    [jerryLi] fix : add new title for login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="main">
     <el-row id="login">
+      <h1 align="center">智能派单停车系统</h1>
       <el-col :span="8" :offset="8">
         <div class="grid-content">
-          <el-button circle class="login_button">登录</el-button>
+          <el-button circle class="login_button"></el-button>
           <LoginForm />
         </div>
       </el-col>
@@ -32,6 +33,8 @@ export default {
   cursor: default;
   font-size: 20px;
   background: url("../assets/head.jpg");
+  background-position: center;
+  background-size: 180% 180%;
 }
 
 .grid-content {
@@ -40,5 +43,12 @@ export default {
 
 #main {
   background: url("../assets/background.jpg");
+}
+
+#login h1 {
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  color: white;
+  margin-left: 100px;
 }
 </style>
