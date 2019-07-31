1c7438dd05b9ddc2df78e8973732b3b2c07274c4
tjslzz <3244188372@qq.com>
2019-07-31 22:22:32 +0800

    [jerryLi] feat : add web socket to progress

--- src/components/mobile/AppointmentShowDetail.vue
+++ src/components/mobile/AppointmentShowDetail.vue
@@ -33,7 +33,7 @@ import StatusBar from "./StatusBar.vue";
 export default {
   data() {
     return {
-      orderDetail: {}
+      orderDetail: { user: {}, parkingLot: {} }
     };
   },
   components: {
@@ -66,22 +66,78 @@ export default {
     this.$get(`/parking-orders/${id}`).then(res => {
       this.orderDetail = res.data;
     });
+    // WebSocket
+    if ("WebSocket" in window) {
+      this.websocket = new WebSocket(
+        "ws://localhost:8080/websocket/" +
+          JSON.parse(localStorage.getItem("user")).userName
+      );
+      this.initWebSocket();
+    } else {
+      alert("当前浏览器 Not support websocket");
+    }
+  },
+  beforeDestroy() {
+    this.onbeforeunload();
   },
   methods: {
     fetchCar: function() {
       this.orderDetail.status = "取车中";
+      let username = this.orderDetail.user.userName;
       this.$put("/parking-orders", this.orderDetail).then(() => {
+        this.$get("/sendOneWebSocket/" + username);
         this.$router.go(-1);
       });
     },
     GivenCar: function() {
       this.orderDetail.status = "待确认";
+      let username = this.orderDetail.user.userName;
       this.$put("/parking-orders", this.orderDetail).then(() => {
+        this.$get("/sendOneWebSocket/" + username);
         this.$router.go(-1);
       });
     },
     seeScore: function() {
       this.$message.success("本次订单的评分是：" + this.orderDetail.score);
+    },
+    //******************************************************************Web Socket********************************************** */
+    initWebSocket() {
+      // 连接错误
+      this.websocket.onerror = this.setErrorMessage;
+
+      // 连接成功
+      this.websocket.onopen = this.setOnopenMessage;
+
+      // 收到消息的回调
+      this.websocket.onmessage = this.setOnmessageMessage;
+
+      // 连接关闭的回调
+      this.websocket.onclose = this.setOncloseMessage;
+
+      // 监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
+      window.onbeforeunload = this.onbeforeunload;
+    },
+    setErrorMessage() {
+      console.log(
+        "WebSocket连接发生错误   状态码：" + this.websocket.readyState
+      );
+    },
+    setOnopenMessage() {
+      console.log("WebSocket连接成功    状态码：" + this.websocket.readyState);
+    },
+    setOnmessageMessage() {
+      // 根据服务器推送的消息做自己的业务处理
+      let userId = JSON.parse(localStorage.getItem("user")).id;
+      this.$store.dispatch("getParkingByUserId", userId);
+    },
+    setOncloseMessage() {
+      console.log("WebSocket连接关闭    状态码：" + this.websocket.readyState);
+    },
+    onbeforeunload() {
+      this.closeWebSocket();
+    },
+    closeWebSocket() {
+      this.websocket.close();
     }
   }
 };
