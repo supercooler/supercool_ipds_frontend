30879e973486a1f657efef78333596119ab075f3
tjslzz <3244188372@qq.com>
2019-07-31 19:32:34 +0800

    [jerryLi] feat : add power judge for user bad operation

--- src/main.js
+++ src/main.js
@@ -7,7 +7,22 @@ import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import Vant from "vant";
 import "vant/lib/index.css";
-
+//权限管理
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(res => res.meta.requireAuth)) {
+    // 验证是否需要登陆
+    if (localStorage.getItem("user")["role"]) {
+      // 查询本地存储信息是否已经登陆
+      next();
+    } else {
+      next({
+        path: "/login" // 未登录则跳转至login页面
+      });
+    }
+  } else {
+    next();
+  }
+});
 Vue.prototype.$get = get;
 Vue.prototype.$post = post;
 Vue.prototype.$delete = myDelete;
